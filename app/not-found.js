b5acbde57606145717dee738d3cb59335ec2b0c8
MohammedOsamma <mo856915i@gmail.com>
2024-10-23 15:00:05 +0200

    Add Error and  Not Found Page

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunalety, we cloud not find the request page or resource. </p>
+    </main>
+  );
+};
+
+export default NotFound;
