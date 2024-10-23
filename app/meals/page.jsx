4f3fd074c355e32cc01b1a994dc2399016b68fff
MohammedOsamma <mo856915i@gmail.com>
2024-10-23 12:36:05 +0200

    Add Database , Meals Components , Loading Component and Fetching Data Dirct from DB (use Better-Sqlite3 Library

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,5 +1,28 @@
-const MealsPage = () => {
-  return <div>page</div>;
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+const MealsPage = async () => {
+  const meals = await getMeals();
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Mael , created
+          <span className={classes.highligth}> by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself . It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
